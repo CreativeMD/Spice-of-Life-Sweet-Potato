30a76205b39ac2428d1c14ef4fa8e8900c8f53e0
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-06-16 13:28:49 +0200

    Added tooltip to food containers

--- src/main/java/com/tarinoita/solsweetpotato/item/foodcontainer/FoodContainerItem.java
+++ src/main/java/com/tarinoita/solsweetpotato/item/foodcontainer/FoodContainerItem.java
@@ -1,10 +1,13 @@
 package com.tarinoita.solsweetpotato.item.foodcontainer;
 
+import java.util.List;
+
 import javax.annotation.Nullable;
 
 import com.tarinoita.solsweetpotato.tracking.FoodList;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -13,6 +16,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
@@ -80,6 +84,12 @@ public class FoodContainerItem extends Item {
             }
         }
         return true;
+    }
+    
+    @Override
+    public void appendHoverText(ItemStack stack, Level level, List<Component> tooltip, TooltipFlag flag) {
+        tooltip.add(Component.translatable("item.solsweetpotato.container.open"));
+        super.appendHoverText(stack, level, tooltip, flag);
     }
     
     @Nullable
